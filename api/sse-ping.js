58176c378993a2ad3efd5205fcc819b067125f2a
Santasu <162797193+Santasu@users.noreply.github.com>
2025-09-08 22:38:27 +0300

    Update sse-ping.js

--- api/sse-ping.js
+++ api/sse-ping.js
@@ -1,17 +1,25 @@
+// /api/sse-ping.js
 module.exports = async (req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/event-stream; charset=utf-8');
   res.setHeader('Cache-Control', 'no-cache, no-transform');
   res.setHeader('Connection', 'keep-alive');
   res.setHeader('X-Accel-Buffering', 'no');
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  try { res.write(':' + ' '.repeat(2048) + '\n'); } catch (_) {}
   res.flushHeaders?.();
 
   let i = 0;
-  const t = setInterval(() => {
+  const timer = setInterval(() => {
     res.write(`event: tick\n`);
     res.write(`data: ${JSON.stringify({ i, at: Date.now() })}\n\n`);
-    if (++i > 50) { clearInterval(t); res.write('data: [DONE]\n\n'); res.end(); }
+    i += 1;
+    if (i > 50) {
+      clearInterval(timer);
+      res.write('data: [DONE]\n\n');
+      res.end();
+    }
   }, 800);
 
-  req.on('close', () => { clearInterval(t); try { res.end(); } catch(_){} });
+  req.on('close', () => { clearInterval(timer); try{res.end();}catch(_){ } });
 };
