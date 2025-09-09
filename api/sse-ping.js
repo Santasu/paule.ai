af396c6da4c4d9b644c99723b63335c74351c736
Santasu <162797193+Santasu@users.noreply.github.com>
2025-09-09 12:01:14 +0300

    Update sse-ping.js

--- api/sse-ping.js
+++ api/sse-ping.js
@@ -1,25 +1,23 @@
-// /api/sse-ping.js
-module.exports = async (req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/event-stream; charset=utf-8');
-  res.setHeader('Cache-Control', 'no-cache, no-transform');
-  res.setHeader('Connection', 'keep-alive');
-  res.setHeader('X-Accel-Buffering', 'no');
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  try { res.write(':' + ' '.repeat(2048) + '\n'); } catch (_) {}
-  res.flushHeaders?.();
+export const config = { runtime: 'nodejs' };
 
-  let i = 0;
-  const timer = setInterval(() => {
-    res.write(`event: tick\n`);
-    res.write(`data: ${JSON.stringify({ i, at: Date.now() })}\n\n`);
-    i += 1;
-    if (i > 50) {
-      clearInterval(timer);
-      res.write('data: [DONE]\n\n');
-      res.end();
-    }
-  }, 800);
+export default async function handler(req, res) {
+  if (req.method === 'OPTIONS') {
+    res.setHeader('Access-Control-Allow-Origin','*');
+    res.setHeader('Access-Control-Allow-Methods','GET, OPTIONS');
+    res.setHeader('Access-Control-Allow-Headers','Content-Type');
+    return res.status(204).end();
+  }
+  if (req.method !== 'GET') return res.status(405).json({ ok:false, error:'Method Not Allowed' });
 
-  req.on('close', () => { clearInterval(timer); try{res.end();}catch(_){ } });
-};
+  res.setHeader('Access-Control-Allow-Origin','*');
+  res.setHeader('Cache-Control','no-cache, no-transform');
+  res.setHeader('Content-Type','text/event-stream; charset=utf-8');
+  res.setHeader('X-Accel-Buffering','no');
+  res.flushHeaders && res.flushHeaders();
+
+  const send = (e,d)=>{ res.write(`event: ${e}\n`); res.write(`data: ${JSON.stringify(d)}\n\n`); };
+
+  let i=0;
+  const t = setInterval(()=> send('ping', { i:i++, t:Date.now() }), 1000);
+  req.on('close', ()=>{ clearInterval(t); try{res.end();}catch(_){ } });
+}
