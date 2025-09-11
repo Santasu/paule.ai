70c81f1f5195d04a9b0dfe6bde8384558483d38e
Santasu <162797193+Santasu@users.noreply.github.com>
2025-09-11 12:22:35 +0300

    Create ping.js

---
+++ api/ping.js
@@ -0,0 +1,20 @@
+export const config = { runtime: 'edge' };
+
+export default async function handler() {
+  const enc = new TextEncoder();
+  const stream = new ReadableStream({
+    start(controller){
+      controller.enqueue(enc.encode('event: ping\ndata: {}\n\n'));
+      const id = setInterval(()=> controller.enqueue(enc.encode('event: ping\ndata: {}\n\n')), 15000);
+      const close = ()=>{ clearInterval(id); try{controller.close();}catch(_){ } };
+      setTimeout(close, 60000);
+    }
+  });
+  return new Response(stream, {
+    headers:{
+      'Content-Type':'text/event-stream; charset=utf-8',
+      'Cache-Control':'no-cache',
+      'Access-Control-Allow-Origin':'*'
+    }
+  });
+}
